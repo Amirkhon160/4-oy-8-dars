c1d30400194f7b91822551094579cd86bf205ccd
Amirkhon160 <amirkhoninagamov001@gmail.com>
2024-07-28 13:49:28 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,39 @@
+// for (let index = 0; index <= 100; index++) {
+//     console.log(index);
+// }
+
+// for (let index2 = 100; index2 >= 0; index2--) {
+//     console.log(index2);
+// }
+
+
+
+const numbers = [2, 4, 9, 10, 45, 67, 8, 90];
+
+for (let index = 0; index < numbers.length; index++) {
+
+    if (numbers[index] % 2 == 0) {
+
+        console.log(numbers[index]);
+
+    }
+}
+
+
+// const names = ["Amirxon", "Akmal", "MirsaidBot", "Lola", "Mirilyos", "ali"]
+
+// const koplar = []
+// const kamlar = []
+
+// for (let index = 0; index < names.length; index++) {
+//     if (names[index].length > 5) {
+//         koplar.push(names[index])
+//     }
+
+//     else {
+//         kamlar.push(names[index])
+//     }
+// }
+
+// console.log(koplar);
+// console.log(kamlar);
